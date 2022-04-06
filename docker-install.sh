50cbcefb6a7dcf60670c03840c0b6f1a000ee989
nithinsaii <nithin.saai@weeve.network>
2022-04-06 21:37:07 +0530

    resolved errors

--- docker-install.sh
+++ docker-install.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+
 if [ -f /etc/fedora-release ]
 then
 distro="fedora"
@@ -7,19 +8,19 @@ then
 distro="centos"
 elif [ -f /etc/os-release ]
 then
-string=$(cat /etc/issue)
-if [[ $string == *"Debian"* ]] || [[ $string == *"Raspbian"* ]]
-then
-distro="debian"
-elif [[ $string == *"Ubuntu"* ]]
-then
-distro="ubuntu"
-fi
+    string=$(cat /etc/issue)
+    if [[ "$string" == *"Debian"* ]] || [[ "$string" == *"Raspbian"* ]]
+    then
+    distro="debian"
+    elif [[ "$string" == *"Ubuntu"* ]]
+    then
+    distro="ubuntu"
+    fi
 fi
 
-echo "The distribution is "$distro
+echo "The distribution is $distro"
 
-if [[ $distro == "fedora" ]] || [[ $distro == "centos" ]]
+if [ "$distro" == "fedora" ] || [ "$distro" == "centos" ]
 then
     package_manager="dnf"
     # utils="dnf-plugins-core"
@@ -27,15 +28,15 @@ then
 else
     package_manager="apt-get"
 fi
-echo "The package manager for "$distro" is " $package_manager
+echo "The package manager for $distro is $package_manager"
 
 # .rpm
 # distro= rhel/centos/fedora
 # package_manager= yum/dnf
 # utils= yum-utils/dnf-plugins-core
-if [[ $distro == "centos" ]] || [[ $distro == "fedora" ]]
+if [[ "$distro" == "centos" ]] || [[ "$distro" == "fedora" ]]
 then
-sudo $package_manager remove docker \
+sudo "$package_manager" remove docker \
     docker-client \
     docker-client-latest \
     docker-common \
@@ -48,9 +49,9 @@ sudo $package_manager remove docker \
     docker-selinux \
     docker-engine-selinux \
     docker-engine
-sudo $package_manager install -y dnf-plugins-core
-sudo $package_manager config-manager --add-repo https://download.docker.com/linux/$distro/docker-ce.repo
-sudo $package_manager install docker-ce docker-ce-cli containerd.io
+sudo "$package_manager" install -y dnf-plugins-core
+sudo "$package_manager" config-manager --add-repo https://download.docker.com/linux/"$distro"/docker-ce.repo
+sudo "$package_manager" install docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo docker run hello-world
 
@@ -64,7 +65,7 @@ sudo apt-get install \
     curl \
     gnupg \
     lsb-release -y
-curl -fsSL https://download.docker.com/linux/$distro/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/"$distro"/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$distro \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -74,5 +75,5 @@ sudo docker run hello-world
 fi
 echo "Adding user to docker group"
 sudo groupadd docker
-sudo usermod -aG docker $USER
+sudo usermod -aG docker "$USER"
 newgrp docker
